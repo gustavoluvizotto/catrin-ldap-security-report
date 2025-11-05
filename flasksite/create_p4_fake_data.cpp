01d2b535b41c5ecfe803dc6a27681cebf6bd2d08
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2025-11-05 12:11:49 +0100

    adding p4 fake data

---
+++ flasksite/create_p4_fake_data.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+
+#pragma pack(push, 1)
+struct switch_t {
+    uint8_t kind;
+    uint8_t length;
+    uint32_t as_number; // AS number
+    uint8_t as_country; // location of AS
+
+    uint8_t as_type:3; // type of AS (e.g. IXP, CDN)
+    uint8_t as_rov_score:1; // ROV Score of AS (1 for 100% or 0 for less)
+    uint8_t as_manrs_member:1; // indicates if AS is participating in MANRS (best routing practices)
+    uint8_t as_mb_risk:2; // middlebox risk level
+    uint8_t padding:1;
+};
+#pragma pack(pop)
+
+int main(int argc, char** argv) {
+    std::cout << "This is a test for creating P4 fake data." << std::endl;
+    std::cout << sizeof(switch_t) << " bytes" << std::endl;
+    switch_t h;
+
+    // scenario 1: up path has high MB risk but participate in MANRS and has ROV score
+    // scenario 2: low path has low MB risk but does not participate in MANRS and has no ROV score
+
+    // AS 1; entry AS; EU; support ROV, MANRS and low MB risk
+    h.kind = 0x72;
+    h.length = 8;
+    h.as_number = 65'537; // 65'536 - 65'551
+    h.as_country = 4;
+    h.as_type = 1;
+    h.as_rov_score = 1; // 0 or 1 (1 for 100% ROV, otherwise 0)
+    h.as_manrs_member = 1; // participating in MANRS
+    h.as_mb_risk = 0; // 0-3 scale;
+    std::ofstream outfile("../research_data/p4_fake_data_as1.bin", std::ios::binary);
+    outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
+    outfile.close();
+
+    // AS 4; exit AS and it must support ROV, MANRS and low MB risk
+    h.kind = 0x72;
+    h.length = 8;
+    h.as_number = 65'538;
+    h.as_country = 6;
+    h.as_type = 2;
+    h.as_rov_score = 1;
+    h.as_manrs_member = 1;
+    h.as_mb_risk = 0;
+    outfile = std::ofstream("../research_data/p4_fake_data_as4.bin", std::ios::binary);
+    outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
+    outfile.close();
+
+    // Upper path (EU)
+    // AS 2; has MANRS and ROV, mild MB risk
+    h.kind = 0x72;
+    h.length = 8;
+    h.as_number = 65'539;
+    h.as_country = 5;
+    h.as_type = 1;
+    h.as_rov_score = 1;
+    h.as_manrs_member = 1;
+    h.as_mb_risk = 1;
+    outfile = std::ofstream("../research_data/p4_fake_data_as2.bin", std::ios::binary);
+    outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
+    outfile.close();
+
+    // AS 3; has MANRS and ROV, moderate MB risk
+    h.kind = 0x72;
+    h.length = 8;
+    h.as_number = 65'540;
+    h.as_country = 19;
+    h.as_type = 1;
+    h.as_rov_score = 1;
+    h.as_manrs_member = 1;
+    h.as_mb_risk = 2;
+    outfile = std::ofstream("../research_data/p4_fake_data_as3.bin", std::ios::binary);
+    outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
+    outfile.close();
+
+    // Lower path (NON-EU)
+    // AS 6; no MANRS, no ROV, low MB risk
+    h.kind = 0x72;
+    h.length = 8;
+    h.as_number = 65'541;
+    h.as_country = 10;
+    h.as_type = 1;
+    h.as_rov_score = 1;
+    h.as_manrs_member = 0;
+    h.as_mb_risk = 0;
+    outfile = std::ofstream("../research_data/p4_fake_data_as6.bin", std::ios::binary);
+    outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
+    outfile.close();
+
+    // AS 7; no MANRS, has ROV, low MB risk
+    h.kind = 0x72;
+    h.length = 8;
+    h.as_number = 65'542;
+    h.as_country = 18;
+    h.as_type = 1;
+    h.as_rov_score = 0;
+    h.as_manrs_member = 0;
+    h.as_mb_risk = 0;
+    outfile = std::ofstream("../research_data/p4_fake_data_as7.bin", std::ios::binary);
+    outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
+    outfile.close();
+
+    return 0;
+}
---
+++ flasksite/read_p4_data.py
@@ -0,0 +1,91 @@
+import struct
+from glob import glob
+
+
+KIND = 114  # 0x72 ALWAYS
+
+# just a few country codes
+US = 1
+CA = 2
+MX = 3
+NL = 4
+DE = 5
+IT = 6
+FR = 7
+GB = 8
+CN = 9
+RU = 10
+IN = 11
+JP = 12
+KR = 13
+AU = 14
+BR = 15
+ZA = 16
+NP = 17
+TR = 18
+AT = 19
+
+
+# https://www.caida.org/catalog/datasets/as-classification/
+TRANSIT_AS = 1  # this includes IXPs, Tier1, Tier2, Tier3
+CDN_AS = 2
+ENTERPRISE_AS = 3
+#STUB_AS
+#MULTIHOMED_AS
+#TIER1_AS
+#TIER2_AS
+#TIER3_AS
+
+# ROV score 0 or 1 (1 for 100% ROV, otherwise 0)
+# MANRS member 0 or 1 (not participating or participating in MANRS)
+# Middlebox risk levels
+# nr_mbs == 0 then LOW RISK
+LOW_RISK = 0  # <=30%
+MILD_RISK = 1  # <=50%
+MODERATE_RISK = 2  # <=75%
+HIGH_RISK = 3  # >75%
+
+def read_p4_data():
+    # Define the struct format (little endian '<' or big endian '>')
+    # '<' = little-endian, standard size (no padding)
+    fmt = '<BBIBB'
+
+    record_size = struct.calcsize(fmt)
+
+    bin_files = glob("../research_data/*.bin")
+
+    p4_data = dict({})
+    for bin_file in bin_files:
+        with open(bin_file, "rb") as f:
+            data = f.read(record_size)
+
+        # Unpack into a tuple
+        kind, length, as_number, as_country, last_byte = struct.unpack(fmt, data)
+        as_type = (last_byte >> 7) & 0x01
+        as_rov_score = (last_byte >> 6) & 0x01
+        as_manrs_member = (last_byte >> 5) & 0x01
+        middlebox_avg_risk_level = (last_byte >> 3) & 0x03
+        padding = last_byte & 0x07
+        p4_data[bin_file] = {
+            "kind": kind,
+            "length": length,
+            "as_number": as_number,
+            "as_country": as_country,
+            "as_type": as_type,
+            "as_rov_score": as_rov_score,
+            "as_manrs_member": as_manrs_member,
+            "middlebox_avg_risk_level": middlebox_avg_risk_level,
+            "padding": padding
+        }
+    return p4_data
+
+def print_p4_binaries_json(p4_json):
+    for bin_file, data in p4_json.items():
+        print(f"File: {bin_file}")
+        for key, value in data.items():
+            print(f"  {key}: {value}")
+
+
+if __name__ == "__main__":
+    p4_json = read_p4_data()
+    print_p4_binaries_json(p4_json)
