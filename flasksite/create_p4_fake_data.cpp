5779b9c7edd04536553de05734e82c1fd736944c
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2025-11-29 18:07:21 +0100

    update mock data

--- flasksite/create_p4_fake_data.cpp
+++ flasksite/create_p4_fake_data.cpp
@@ -6,14 +6,15 @@
 struct switch_t {
     uint8_t kind;
     uint8_t length;
-    uint32_t as_number; // AS number
+    uint32_t as_number; // AS number; 65'536 - 65'551
     uint8_t as_country; // location of AS
 
     uint8_t as_type:3; // type of AS (e.g. IXP, CDN)
     uint8_t as_rov_score:1; // ROV Score of AS (1 for 100% or 0 for less)
-    uint8_t as_manrs_member:1; // indicates if AS is participating in MANRS (best routing practices)
-    uint8_t as_mb_risk:2; // middlebox risk level
-    uint8_t padding:1;
+    uint8_t as_manrs_member:1; // indicates if AS is participating in MANRS (best routing practices); 1 - member, 0 - non-member
+    uint8_t as_mb_risk:2; // middlebox risk level 0-3 scale;
+    //uint8_t padding:1;
+    uint8_t as_ldap_risk:1; // LDAP risk level: 0 - no risk, 1 - risk
 };
 #pragma pack(pop)
 
@@ -22,18 +23,24 @@ int main(int argc, char** argv) {
     std::cout << sizeof(switch_t) << " bytes" << std::endl;
     switch_t h;
 
-    // scenario 1: up path has high MB risk but participate in MANRS and has ROV score
-    // scenario 2: low path has low MB risk but does not participate in MANRS and has no ROV score
+    // scenario 1: up path has high MB risk and no LDAP risk but participate in MANRS and has ROV score
+    // scenario 2: low path has low MB risk and has LDAP risk but does not participate in MANRS and has no ROV score
+
+    // paths
+    //       --- AS2 --- AS3 ---
+    //     /                     \
+    // AS1 ---- AS6 --- AS7 ------ AS4
 
     // AS 1; entry AS; EU; support ROV, MANRS and low MB risk
     h.kind = 0x72;
     h.length = 8;
-    h.as_number = 65'537; // 65'536 - 65'551
+    h.as_number = 65'537;
     h.as_country = 4;
     h.as_type = 1;
-    h.as_rov_score = 1; // 0 or 1 (1 for 100% ROV, otherwise 0)
-    h.as_manrs_member = 1; // participating in MANRS
-    h.as_mb_risk = 0; // 0-3 scale;
+    h.as_rov_score = 1;
+    h.as_manrs_member = 1;
+    h.as_mb_risk = 0;
+    h.as_ldap_risk = 0;
     std::ofstream outfile("../research_data/p4_fake_data_as1.bin", std::ios::binary);
     outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
     outfile.close();
@@ -47,6 +54,7 @@ int main(int argc, char** argv) {
     h.as_rov_score = 1;
     h.as_manrs_member = 1;
     h.as_mb_risk = 0;
+    h.as_ldap_risk = 0;
     outfile = std::ofstream("../research_data/p4_fake_data_as4.bin", std::ios::binary);
     outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
     outfile.close();
@@ -61,6 +69,7 @@ int main(int argc, char** argv) {
     h.as_rov_score = 1;
     h.as_manrs_member = 1;
     h.as_mb_risk = 1;
+    h.as_ldap_risk = 0;
     outfile = std::ofstream("../research_data/p4_fake_data_as2.bin", std::ios::binary);
     outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
     outfile.close();
@@ -74,6 +83,7 @@ int main(int argc, char** argv) {
     h.as_rov_score = 1;
     h.as_manrs_member = 1;
     h.as_mb_risk = 2;
+    h.as_ldap_risk = 0;
     outfile = std::ofstream("../research_data/p4_fake_data_as3.bin", std::ios::binary);
     outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
     outfile.close();
@@ -88,6 +98,7 @@ int main(int argc, char** argv) {
     h.as_rov_score = 1;
     h.as_manrs_member = 0;
     h.as_mb_risk = 0;
+    h.as_ldap_risk = 1;
     outfile = std::ofstream("../research_data/p4_fake_data_as6.bin", std::ios::binary);
     outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
     outfile.close();
@@ -101,6 +112,7 @@ int main(int argc, char** argv) {
     h.as_rov_score = 0;
     h.as_manrs_member = 0;
     h.as_mb_risk = 0;
+    h.as_ldap_risk = 0;
     outfile = std::ofstream("../research_data/p4_fake_data_as7.bin", std::ios::binary);
     outfile.write(reinterpret_cast<char*>(&h), sizeof(h));
     outfile.close();
